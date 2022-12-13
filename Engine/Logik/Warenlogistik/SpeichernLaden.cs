b8fd7f632a032ea516748f555ff2ceda8856f4da
Discipolus <t.labinnah@web.de>
2022-12-13 11:15:31 +0100

    Datenbank Verbindung optionalisiert.

--- Engine/Logik/Warenlogistik/SpeichernLaden.cs
+++ Engine/Logik/Warenlogistik/SpeichernLaden.cs
@@ -16,6 +16,7 @@ namespace Engine.Logik.Warenlogistik
         private static readonly string DateinameKatalog = "Katalog.xml";
         private static readonly string DateinameLagerhäuser = "Lagerhäuser.xml";
         private static readonly string connectionString = @"Data Source=BIGGREEN;Initial Catalog=WWSystem;User ID=WWSystemZugriff;Password=AbC12Test";
+        //private static readonly string connectionString = @"Data Source=FVTOGNB0146;Initial Catalog=WWSystem;User ID=WWSystemZugriff;Password=AbC12Test"; //FVTOGNB0146\SQLEXPRESS
         private static SqlConnection conn = new SqlConnection(connectionString);
 
         static SpeichernLaden()
@@ -23,20 +24,27 @@ namespace Engine.Logik.Warenlogistik
         }
         internal static void KatalogSpeichern(List<KatalogItem> katalog)
         {
-            //Speichern<List<KatalogItem>>(katalog, DateinameKatalog);
-            string sqlvalues = katalog[0].ToSqlValue();
-            for(int i = 1; i < katalog.Count; i++)
-            {                
-                sqlvalues += ", " + katalog[i].ToSqlValue();
+            try
+            {
+                string sqlvalues = katalog[0].ToSqlValue();
+                for (int i = 1; i < katalog.Count; i++)
+                {
+                    sqlvalues += ", " + katalog[i].ToSqlValue();
+                }
+                string sql = "Insert into Produkte (Name, MaßeX, MaßeY, MaßeZ, Guid, Lagerhäuser) values " + sqlvalues + ";";
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                conn.Open();
+                adapter.InsertCommand = new SqlCommand(sql, conn);
+                adapter.InsertCommand.ExecuteNonQuery();
+                conn.Close();
+                adapter.Dispose();
             }
-            string sql = "Insert into Produkte (Name, MaßeX, MaßeY, MaßeZ, Guid, Lagerhäuser) values " + sqlvalues + ";";
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            conn.Open();
-            adapter.InsertCommand = new SqlCommand(sql, conn);
-            adapter.InsertCommand.ExecuteNonQuery();
-            conn.Close();
-            adapter.Dispose();
-            
+            catch
+            {
+                string message = "Katalog konnte nicht in SQL Datenbank gespeichert werden. Speichere lokal.";
+                Console.WriteLine(message);
+                Speichern<List<KatalogItem>>(katalog, DateinameKatalog);
+            }
         }
         internal static void LagerhäuserSpeichern(List<Lagerhaus> lagerhäuser)
         {
@@ -54,33 +62,41 @@ namespace Engine.Logik.Warenlogistik
 
         }
         internal static List<KatalogItem>? KatalogLaden()
-        {            
-            string sql = "Select * from Produkte";
-            conn.Open();
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            List<KatalogItem> list = new List<KatalogItem>();
-            //(Name, MaßeX, MaßeY, MaßeZ, Guid, Lagerhäuser)
-            while (reader.Read())
+        {
+            try
             {
-                string name = (string)reader.GetValue(0);
-                MaßeTemplate maße = new MaßeTemplate((double)reader.GetValue(1), (double)reader.GetValue(2), (double)reader.GetValue(3));
-                Guid GUID = (Guid)reader.GetValue(4);
-                List<int> lagerhäuser = new List<int>();
-                foreach (string s in ((string)reader.GetValue(5)).Split(";"))
-                {
-                    int item;
-                    if (int.TryParse(s, out item))
-                    {
-                        lagerhäuser.Add(item);
-                    }
-                }
+                string sql = "Select * from Produkte";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
 
-                list.Add(new KatalogItem(name, maße, lagerhäuser, GUID));
+                List<KatalogItem> list = new List<KatalogItem>();
+                //(Name, MaßeX, MaßeY, MaßeZ, Guid, Lagerhäuser)
+                while (reader.Read())
+                {
+                    string name = (string)reader.GetValue(0);
+                    MaßeTemplate maße = new MaßeTemplate((double)reader.GetValue(1), (double)reader.GetValue(2), (double)reader.GetValue(3));
+                    Guid GUID = (Guid)reader.GetValue(4);
+                    List<int> lagerhäuser = new List<int>();
+                    foreach (string s in ((string)reader.GetValue(5)).Split(";"))
+                    {
+                        int item;
+                        if (int.TryParse(s, out item))
+                        {
+                            lagerhäuser.Add(item);
+                        }
+                    }
+
+                    list.Add(new KatalogItem(name, maße, lagerhäuser, GUID));
+                }
+                return list;
             }
-            return list;
-            //return Laden<List<KatalogItem>>(DateinameKatalog);
+            catch
+            {
+                string message = "Katalog konnte nicht aus SQL Datenbank geladen werden. Versuche lokale Datei zu laden.";
+                Console.WriteLine(message);
+                return Laden<List<KatalogItem>>(DateinameKatalog);
+            }
         }
         internal static List<Lagerhaus>? LagerhäuserLaden()
         {
