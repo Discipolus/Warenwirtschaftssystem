2d4c0f62bcf5e5896c48a905a80505cf005edd77
Vincent H <T.Labinnah@web.de>
2023-01-18 07:13:31 +0100

    KatalogItemListItem hinzugefuegt

--- Engine/Konstrukte/MaßeTemplate.cs
+++ Engine/Konstrukte/MaßeTemplate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine.Logik.Warenlogistik;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -26,6 +27,9 @@ namespace Engine.Konstrukte
         public MaßeTemplate(double[] maße) : this(maße[0], maße[1], maße[2]){}
         #endregion
 
+        private MaßeTemplate? backupData;
+        private bool inBearbeitung = false;
+
         public override string ToString()
         {
             return "(" + X + "|" + Y + "|" + Z + ")";
@@ -34,6 +38,34 @@ namespace Engine.Konstrukte
         {
             return new double[3] { X, Y, Z };
         }
-        
+
+        public void BeginEdit()
+        {
+            if (!inBearbeitung)
+            {
+                inBearbeitung = true;
+                this.backupData = new MaßeTemplate(this.X, this.Y, this.Z);
+            }            
+        }
+
+        public void CancelEdit()
+        {
+            if (inBearbeitung)
+            {
+                this.X = this.backupData.X;
+                this.Y = this.backupData.Y;
+                this.Z = this.backupData.Z;
+                inBearbeitung = false;
+            }
+        }
+
+        public void EndEdit()
+        {
+            if (inBearbeitung)
+            {
+                this.backupData = null;
+                inBearbeitung = false;
+            }
+        }
     }
 }
---
+++ GUI_WPF/ViewModels/KatalogItemListItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Logik.Warenlogistik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_WPF.ViewModels
+{
+    internal class KatalogItemListItemViewModel : Prism.Mvvm.BindableBase
+    {
+        #region Properties
+        KatalogItem KatalogItem { get; set; }
+        #endregion
+        KatalogItemListItemViewModel()
+        {
+
+        }
+    }
+}
